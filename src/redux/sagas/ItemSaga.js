a67c037b08a7c5ef4250291ec36de35a9e2393d2
Cooper Miller <coopermllr@gmail.com>
2018-11-15 13:16:11 -0600

    update

--- src/redux/sagas/ItemSaga.js
+++ src/redux/sagas/ItemSaga.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { call, put, takeEvery } from 'redux-saga/effects';
 
-
+// add Item to database 
 function* addItem(action) {
     console.log('Add Item generator: ', action);
     try {
@@ -11,7 +11,7 @@ function* addItem(action) {
     catch(error) {
       console.log('Error in adding item generator', error);
     }
-  }
+}
 
 // get items on shelf from database
 function* fetchItems(action) {
@@ -23,17 +23,12 @@ function* fetchItems(action) {
     catch(error) {
       console.log('Error in fetch items generator', error);
     }
-  }
-
-// function* userItems(action) {
-//     console.log('In userItem gener');
-    
-// }
+}
 
 function* itemSaga() {
     yield takeEvery('FETCH_ITEMS', fetchItems);
     yield takeEvery('ADD_ITEM', addItem);
-    yield takeEvery('FETCH_USER_ITEMS', userItems);
+    // yield takeEvery('FETCH_USER_ITEMS', userItems);
 }
 
 export default itemSaga;
