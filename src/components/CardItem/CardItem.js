0d45fa5964a90d24ae257347bdbf07a08684a806
Cooper Miller <coopermllr@gmail.com>
2018-11-17 14:35:12 -0600

    bug fix

--- src/components/CardItem/CardItem.js
+++ src/components/CardItem/CardItem.js
@@ -37,7 +37,7 @@ class CardItem extends Component {
   return (
     <div>
         {this.props.reduxState.shelf.map( item =>(
-          <section className="display" >
+          <section key={item.id} className="display" >
           <Card id="card" key={item.id}>
             <CardActionArea>
               <CardMedia
